57d92a9013e3bb9ea90c5422ebe67934a5392cc3
yuzuhakuon <yuzuhakuon@gmail.com>
2022-03-10 17:07:29 +0800

    feat: Path Copy Copy
    generate various file paths

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { PathCopyCopyController } from './controllers/pathCopyController';
 import { ProjectController } from './controllers/projectController';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(new ProjectController(context));
+	context.subscriptions.push(new PathCopyCopyController(context));
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
